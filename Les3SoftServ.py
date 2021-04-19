a4d2d84874e2da4ea76c90e0bc2dadb503baf07d
Oleh Romanyshyn <Oleh60@gmail.com>
2021-04-19 22:34:24 +0300

    FirstCommit

---
+++ Les3SoftServ.py
@@ -0,0 +1,69 @@
+
+
+#Home Work
+#phil = ("Beautiful is better than ugly.\
+#Explicit is better than implicit.\
+#Simple is better than complex.\
+#Complex is better than complicated.\
+#Readability counts.")
+#count = phil.count("better")
+#count1 = phil.count("never")
+#count2 = phil.count("is")
+#print("'better' found", count, "times")
+#print("'never' found", count1, "times")
+#print("'is' found", count2, "times")
+
+# print(phil.upper())
+#
+# print(phil.replace("i", "&"))
+
+#n = int(input("Введіть число: "))
+#
+# # a = int(n[0])
+# # b = int(n[1])
+# # c = int(n[2])
+# # d = int(n[3])
+# # print("Добуток числа = ", a*b*c*d )
+# a = n//1000
+# b = (n - a*1000)//100
+#
+# print(a,b)
+
+
+
+
+
+
+# # n1 = n[::-1]
+# # print("Зворотнє число: ", n1)
+#
+#
+# # n1 = list(n)
+# # n1.reverse()
+# # n3 = "".join(n1)
+# # print("Зворотнє число", n3)
+# n1 =sorted(n)
+# print(n1)
+# a = 3
+# b = 4
+# a, b = b,a
+# print(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
